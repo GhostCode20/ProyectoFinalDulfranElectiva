d74f45f3abc90b429650f21ab29e4d16f8a03867
shary casianis <100879573+GhostCode20@users.noreply.github.com>
2023-11-07 17:38:17 -0500

    1

---
+++ ClsFincaNov07.java
@@ -0,0 +1,152 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Karina Peña
+ */
+public class ClsFincaNov07 {
+    
+        private String str_nit;
+        private String str_nombre;
+        private String str_ubicacion;
+        private String str_prop;
+        private String str_correo;
+        
+        private String str_cod;
+        private String str_nombre2;
+        private String str_produccion;
+        private String str_descripcion;
+        
+        private String str_cod2;
+        private String str_nombre3;
+        private String str_contacto;
+        private String str_cargo;
+
+    public ClsFincaNov07(String str_nit, String str_nombre, String str_ubicacion, String str_prop, String str_correo, String str_cod, String str_nombre2, String str_produccion, String str_descripcion, String str_cod2, String str_nombre3, String str_contacto, String str_cargo) {
+        this.str_nit = str_nit;
+        this.str_nombre = str_nombre;
+        this.str_ubicacion = str_ubicacion;
+        this.str_prop = str_prop;
+        this.str_correo = str_correo;
+        this.str_cod = str_cod;
+        this.str_nombre2 = str_nombre2;
+        this.str_produccion = str_produccion;
+        this.str_descripcion = str_descripcion;
+        this.str_cod2 = str_cod2;
+        this.str_nombre3 = str_nombre3;
+        this.str_contacto = str_contacto;
+        this.str_cargo = str_cargo;
+    }
+
+    public String getStr_nit() {
+        return str_nit;
+    }
+
+    public String getStr_nombre() {
+        return str_nombre;
+    }
+
+    public String getStr_ubicacion() {
+        return str_ubicacion;
+    }
+
+    public String getStr_prop() {
+        return str_prop;
+    }
+
+    public String getStr_correo() {
+        return str_correo;
+    }
+
+    public String getStr_cod() {
+        return str_cod;
+    }
+
+    public String getStr_nombre2() {
+        return str_nombre2;
+    }
+
+    public String getStr_produccion() {
+        return str_produccion;
+    }
+
+    public String getStr_descripcion() {
+        return str_descripcion;
+    }
+
+    public String getStr_cod2() {
+        return str_cod2;
+    }
+
+    public String getStr_nombre3() {
+        return str_nombre3;
+    }
+
+    public String getStr_contacto() {
+        return str_contacto;
+    }
+
+    public String getStr_cargo() {
+        return str_cargo;
+    }
+
+    public void setStr_nit(String str_nit) {
+        this.str_nit = str_nit;
+    }
+
+    public void setStr_nombre(String str_nombre) {
+        this.str_nombre = str_nombre;
+    }
+
+    public void setStr_ubicacion(String str_ubicacion) {
+        this.str_ubicacion = str_ubicacion;
+    }
+
+    public void setStr_prop(String str_prop) {
+        this.str_prop = str_prop;
+    }
+
+    public void setStr_correo(String str_correo) {
+        this.str_correo = str_correo;
+    }
+
+    public void setStr_cod(String str_cod) {
+        this.str_cod = str_cod;
+    }
+
+    public void setStr_nombre2(String str_nombre2) {
+        this.str_nombre2 = str_nombre2;
+    }
+
+    public void setStr_produccion(String str_produccion) {
+        this.str_produccion = str_produccion;
+    }
+
+    public void setStr_descripcion(String str_descripcion) {
+        this.str_descripcion = str_descripcion;
+    }
+
+    public void setStr_cod2(String str_cod2) {
+        this.str_cod2 = str_cod2;
+    }
+
+    public void setStr_nombre3(String str_nombre3) {
+        this.str_nombre3 = str_nombre3;
+    }
+
+    public void setStr_contacto(String str_contacto) {
+        this.str_contacto = str_contacto;
+    }
+
+    public void setStr_cargo(String str_cargo) {
+        this.str_cargo = str_cargo;
+    }
+        
+        
+        
+        
+    
+}
